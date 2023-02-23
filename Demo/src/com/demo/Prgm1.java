9f1034510bbbc9884de4a039d5e2489ef718eeb1
H S Darshini <hdarshini@BLR.ALLEGISINDIA.COM>
2023-02-23 15:44:02 +0530

    completed

---
+++ Demo/src/com/demo/Prgm1.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class Prgm1 {
+	public static void main(String args[]) {
+		System.out.println("hello");
+	}
+
+}
---
+++ Demo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hdarshini
+ *
+ */
+module Demo {
+}
